dffb81d109de07b5f052901c21994b4a94b7f2d8
LiesmannManda <126512206+LiesmannManda@users.noreply.github.com>
2023-08-12 08:17:39 +0100

    Update wBSFLIX.py

--- wBSFLIX.py
+++ wBSFLIX.py
@@ -69,6 +69,14 @@ if movie_search_query:
     else:
         st.write("No movies found!")
 
+# Display top popular movies based on number of ratings
+st.subheader("Top Popular Movies")
+movie_ratings_count = ratings_df.groupby('movieId').size().reset_index(name='num_ratings')
+movie_ratings_count = movie_ratings_count.merge(movies_df[['movieId', 'title']], on='movieId')
+top_movies = movie_ratings_count.sort_values(by="num_ratings", ascending=False).head(10)
+for index, row in top_movies.iterrows():
+    st.write(row['title'])
+
 # Collaborative Filtering Recommendations
 st.subheader("Collaborative Filtering Recommendations")
 selected_movie_title = st.selectbox("Select a movie you like:", movies_df['title'].tolist())
