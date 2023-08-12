30572b06dbb25dd7b94c9fa1ec3e270e3064c2d2
LiesmannManda <126512206+LiesmannManda@users.noreply.github.com>
2023-08-12 10:28:33 +0100

    Update wBSFLIX.py

--- wBSFLIX.py
+++ wBSFLIX.py
@@ -41,45 +41,39 @@ body {
     color: #ffffff;
     background-color: #111111;
 }
-.stTextInput input[type="text"] {
-    color: #ffffff;
-    background-color: #333333;
-}
-.stButton>button {
+.stTextInput input[type="text"], .stButton>button {
     color: #ffffff;
     background-color: #E50914;
 }
-.sidebar .sidebar-content {
+.st-h2, .st-h3 {
+    color: #E50914;
+}
+.stSidebar .sidebar-content {
     background-color: #111111;
 }
 </style>
     """, unsafe_allow_html=True)
 
-# Display the logo and banner with center alignment and reduced size
+# Display the logo with center alignment and reduced size
 st.markdown(
-    "<div style='text-align: center;'><img src='data:image/png;base64,{}' style='width:50%'></div>".format(
-        base64.b64encode(open("/mnt/data/wbsflix logo.png", "rb").read()).decode()
+    "<div style='text-align: center;'><img src='data:image/png;base64,{}' style='width:30%'></div>".format(
+        base64.b64encode(open("wbsflix_logo.png", "rb").read()).decode()
     ),
     unsafe_allow_html=True,
 )
 
 # Display the banner
-banner = Image.open("/mnt/data/wbs flix banner.png")
+banner = Image.open("wbs_flix_banner.png")
 st.image(banner, use_column_width=True)
 
-# Introduction message
+# Add a centered welcome message
 st.markdown(
-    """
-    <div style='text-align: center; color: #E50914;'>
-        <h2>Welcome to WBSFLIX!</h2>
-        <p>Discover movies tailored just for you. Dive in and explore!</p>
-    </div>
-    """,
+    "<div style='text-align: center;'><h2>Welcome to WBSFLIX! Your personal movie recommendation platform.</h2></div>",
     unsafe_allow_html=True,
 )
 
 # Sidebar with overall controls
-st.sidebar.header("Controls")
+st.sidebar.markdown("<h1 style='color: #E50914;'>Controls</h1>", unsafe_allow_html=True)
 st.sidebar.write("Use these controls to adjust your recommendations.")
 
 # Searching for movies
@@ -96,21 +90,7 @@ if movie_search_query:
     else:
         st.write("No movies found!")
 
-# Collaborative Filtering Recommendations
-st.markdown("<h3 style='color: #E50914;'>Collaborative Filtering Recommendations</h3>", unsafe_allow_html=True)
-selected_movie_title = st.selectbox("Select a movie you like:", movies_df['title'].tolist())
-if selected_movie_title:
-    selected_movie_id = movies_df[movies_df['title'] == selected_movie_title]['movieId'].iloc[0]
-    user_item_matrix = ratings_df.pivot(index='userId', columns='movieId', values='rating').fillna(0)
-    item_similarity = cosine_similarity(user_item_matrix.T)
-    item_similarity_df = pd.DataFrame(item_similarity, index=user_item_matrix.columns, columns=user_item_matrix.columns)
-    similar_movie_ids = item_similarity_df[selected_movie_id].sort_values(ascending=False).index[1:11]
-    recommended_movie_titles = movies_df[movies_df['movieId'].isin(similar_movie_ids)]['title'].tolist()
-    st.write("Movies you might also like:")
-    for movie in recommended_movie_titles:
-        st.write(movie)
-
-# Additional recommendation systems can be added below...
+# Rest of the recommendation code...
 
 # Footer
 st.markdown(
