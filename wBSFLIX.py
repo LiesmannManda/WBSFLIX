ab603f22f86d7dde2c8cf24e4ba1023eddb655b0
LiesmannManda <126512206+LiesmannManda@users.noreply.github.com>
2023-08-12 07:07:12 +0100

    Update wBSFLIX.py

--- wBSFLIX.py
+++ wBSFLIX.py
@@ -46,22 +46,16 @@ body {
 </style>
     """, unsafe_allow_html=True)
 
-# Display the logo
-st.image("/mnt/data/wbsflix logo.png", use_column_width=True)
-
 st.title("WBSFLIX Movie Recommender")
+
+# Display logo
+with open("/mnt/data/wbsflix logo.png", "rb") as f:
+    logo_image = f.read()
+st.image(logo_image, use_column_width=True)
 
 # Sidebar with overall controls
 st.sidebar.header("Controls")
 st.sidebar.write("Use these controls to adjust your recommendations.")
-
-# Display top popular movies based on number of ratings
-st.subheader("Top Popular Movies")
-movie_ratings_count = ratings_df.groupby('movieId').size().reset_index(name='num_ratings')
-movie_ratings_count = movie_ratings_count.merge(movies_df[['movieId', 'title']], on='movieId')
-top_movies = movie_ratings_count.sort_values(by="num_ratings", ascending=False).head(10)
-for index, row in top_movies.iterrows():
-    st.write(row['title'])
 
 # Searching for movies
 movie_search_query = st.sidebar.text_input("Search for a movie by title:", "")
@@ -76,6 +70,14 @@ if movie_search_query:
             st.image(poster_url)
     else:
         st.write("No movies found!")
+
+# Display top popular movies based on number of ratings
+st.subheader("Top Popular Movies")
+movie_ratings_count = ratings_df.groupby('movieId').size().reset_index(name='num_ratings')
+movie_ratings_count = movie_ratings_count.merge(movies_df[['movieId', 'title']], on='movieId')
+top_movies = movie_ratings_count.sort_values(by="num_ratings", ascending=False).head(10)
+for index, row in top_movies.iterrows():
+    st.write(row['title'])
 
 # Collaborative Filtering Recommendations
 st.subheader("Collaborative Filtering Recommendations")
